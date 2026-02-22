66c37ca1e61abe57f61e8b3801d82db4b8fa61eb
sgraju <sgraju.tibco@gmail.com>
2026-02-22 17:14:38 +0400

    roboshop-dev-infra

--- 10-sg/main.tf
+++ 10-sg/main.tf
@@ -1,9 +1,19 @@
-module "catalogue" {
-  source = "terraform-aws-modules/security-group/aws"
+# module "catalogue" {
+#   source = "terraform-aws-modules/security-group/aws"
 
-  name  = "${local.common_name_suffix}-catalogue" # roboshop-dev-catalogue
-  use_name_prefix = false
-  description = "Security group for catalogue with custom ports open within VPC, egress all traffic"
-  vpc_id      = data.aws_ssm_parameter.vpc_id.value
+#   name  = "${local.common_name_suffix}-catalogue" # roboshop-dev-catalogue
+#   use_name_prefix = false
+#   description = "Security group for catalogue with custom ports open within VPC, egress all traffic"
+#   vpc_id      = data.aws_ssm_parameter.vpc_id.value
 
+# }
+
+module "sg" {
+    count = length(var.sg_names)
+    source = "git::https://github.com/sgraju/terraform-aws-sg.git?ref=main"
+    project_name = var.project_name
+    environment = var.environment
+    sg_name = var.sg_names[count.index]
+    sg_description = "Created for ${var.sg_names[count.index]}"
+    vpc_id = local.vpc_id
 }

05789c823c3e853f79bd566ab1f7a4d6821ecb45
sgraju <sgraju.tibco@gmail.com>
2026-02-23 06:52:59 +0400

    roboshop-dev-infra

--- 10-sg/main.tf
+++ 10-sg/main.tf
@@ -17,3 +17,13 @@ module "sg" {
     sg_description = "Created for ${var.sg_names[count.index]}"
     vpc_id = local.vpc_id
 }
+
+# Frontend accepting traffic from Frontend ALB
+resource "aws_security_group_rule" "frontend_frontend_alb" {
+  type = "ingress"
+  security_group_id = module.sg[9].sg_id # frontend SG ID
+  source_security_group_id  = module.sg[11].sg_id
+  from_port  = 80
+  protocol   = "tcp"
+  to_port    = 80
+}

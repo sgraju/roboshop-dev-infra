337da188ca618cb61c0cdcf809cda5351eec2951
sgraju <sgraju.tibco@gmail.com>
2026-02-24 22:15:15 +0400

    roboshop-dev-infra

---
+++ 40-databases/main.tf
@@ -0,0 +1,19 @@
+resource "aws_instance" "mongodb" {
+  ami = local.ami_id
+  instance_type = "t3.micro"
+  vpc_security_group_ids = [local.database_sg_id]
+  subnet_id = local.database_subnet_id
+  
+  tags = merge (
+    local.common_tags,
+    {
+    Name = "${local.common_name_suffix}-mongodb"
+  }
+  )
+
+  provisioner "remote-exec" {
+    
+  }
+}
+
+ 
--- 50-backend-alb/locals.tf
+++ 50-backend-alb/locals.tf
@@ -3,6 +3,7 @@ locals {
   vpc_id = data.aws_ssm_parameter.vpc_id.value
   backend_alb_sg_id = data.aws_ssm_parameter.backend_alb_sg_id.value
   private_subnet_ids = split("," , data.aws_ssm_parameter.private_subnet_ids.value)
+  publicsubnet_ids = split("," , data.aws_ssm_parameter.public_subnet_ids.value)
   common_tags = {
     Project = var.project_name
     Environment = var.environment

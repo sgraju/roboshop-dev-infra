bbd154a837fa1e10748ad20459f35ba289c54d7f
sgraju <sgraju.tibco@gmail.com>
2026-02-25 13:57:32 +0400

    roboshop-dev-infra

--- 40-databases/main.tf
+++ 40-databases/main.tf
@@ -1,19 +1,34 @@
 resource "aws_instance" "mongodb" {
   ami = local.ami_id
   instance_type = "t3.micro"
-  vpc_security_group_ids = [local.database_sg_id]
+  vpc_security_group_ids = [local.mongodb_sg_id]
   subnet_id = local.database_subnet_id
   
   tags = merge (
     local.common_tags,
     {
-    Name = "${local.common_name_suffix}-mongodb"
+    Name = "${local.common_name_suffix}-mongodb" # roboshop-dev-mongodb
   }
   )
-
-  provisioner "remote-exec" {
-    
-  }
 }
+
+  resource "terraform_data" "mongodb" {
+    triggers_replace = [
+      aws_instance.mongodb.id,
+    ]
+
+    connection {
+      type = "ssh"
+      user = "ec2-user"
+      password = "DevOps321"
+      host = aws_instance.mongodb.private_ip
+    }
+    provisioner "remote-exec" {
+      inline = [
+        "echo Hello world"
+      ]
+    }
+}
+    
 
  

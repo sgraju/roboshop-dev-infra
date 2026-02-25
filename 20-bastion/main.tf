5c2bbc894b07912afe972c98c69da5336d6191b1
sgraju <sgraju.tibco@gmail.com>
2026-02-25 22:04:50 +0400

    roboshop-dev-infra

--- 20-bastion/main.tf
+++ 20-bastion/main.tf
@@ -3,7 +3,7 @@ resource "aws_instance" "bastion" {
   instance_type = "t3.micro"
   vpc_security_group_ids = [local.bastion_sg_id]
   subnet_id = local.public_subnet_id
-  
+  iam_instance_profile = aws_iam_instance_profile.bastion.name
   user_data = file("bastion.sh")
   tags = merge (
     local.common_tags,
@@ -13,4 +13,7 @@ resource "aws_instance" "bastion" {
   )
 }
 
- 
+resource "aws_iam_instance_profile" "bastion" {
+  name = "mysql"
+  role = "BastionTerraformAdmin"
+}

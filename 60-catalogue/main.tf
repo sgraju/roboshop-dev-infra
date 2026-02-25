3d49ae2293ac00a6dbc7f913e329e25936f666c7
sgraju <sgraju.tibco@gmail.com>
2026-02-25 22:30:09 +0400

    roboshop-dev-infra

--- 60-catalogue/main.tf
+++ 60-catalogue/main.tf
@@ -1,13 +1,15 @@
+# Create EC2 instance
+
 resource "aws_instance" "mongodb" {
   ami = local.ami_id
   instance_type = "t3.micro"
-  vpc_security_group_ids = [local.mongodb_sg_id]
-  subnet_id = local.database_subnet_id
+  vpc_security_group_ids = [local.catalogue_sg_id]
+  subnet_id = local.private_subnet_id
   
   tags = merge (
     local.common_tags,
     {
-    Name = "${local.common_name_suffix}-mongodb" # roboshop-dev-mongodb
+    Name = "${local.common_name_suffix}-catalogue" # roboshop-dev-mongodb
   }
   )
 }

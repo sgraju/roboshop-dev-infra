793c96e6b123e11605a51d67ac04256d2d7a22a4
sgraju <sgraju.tibco@gmail.com>
2026-02-25 17:06:13 +0400

    roboshop-dev-infra

--- 40-databases/main.tf
+++ 40-databases/main.tf
@@ -117,3 +117,43 @@ resource "aws_instance" "rabbitmq" {
       ]
     }
 }
+
+resource "aws_instance" "mysql" {
+  ami = local.ami_id
+  instance_type = "t3.micro"
+  vpc_security_group_ids = [local.mysql_sg_id]
+  subnet_id = local.database_subnet_id
+  
+  tags = merge (
+    local.common_tags,
+    {
+    Name = "${local.common_name_suffix}-mysql" # roboshop-dev-mongodb
+  }
+  )
+}
+
+  resource "terraform_data" "mysql" {
+    triggers_replace = [
+      aws_instance.mysql.id
+    ]
+
+    connection {
+      type = "ssh"
+      user = "ec2-user"
+      password = "DevOps321"
+      host = aws_instance.mysql.private_ip
+    }
+
+# terraform copies this file to mongodb server
+    provisioner "file" {
+      source = "bootstrap.sh"
+      destination = "/tmp/bootstrap.sh"
+    }
+
+    provisioner "remote-exec" {
+      inline = [
+        "chmod +x /tmp/bootstrap.sh",
+        "sudo sh /tmp/bootstrap.sh mysql"
+      ]
+    }
+}

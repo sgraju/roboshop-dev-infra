feb5a30a15ab183e57b2808103eb8302e4208770
sgraju <sgraju.tibco@gmail.com>
2026-02-25 20:40:40 +0400

    roboshop-dev-infra

--- 40-databases/bootstrap.sh
+++ 40-databases/bootstrap.sh
@@ -25,4 +25,4 @@ else
     cd $ANSIBLE_DIR
 fi
 
-ansible-playbook -e component=$component -e environment=$environment main.yaml
+ansible-playbook -e component=$component -e env=$environment main.yaml

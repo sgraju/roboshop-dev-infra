705704ee9b7249efcdb412e5b1ed5e835cd21797
sgraju <sgraju.tibco@gmail.com>
2026-02-25 14:25:57 +0400

    roboshop-dev-infra

--- 30-sg-rules/locals.tf
+++ 30-sg-rules/locals.tf
@@ -2,6 +2,7 @@ locals {
     backend_alb_sg_id = data.aws_ssm_parameter.backend_alb_sg_id.value
     frontend_alb_sg_id = data.aws_ssm_parameter.frontend_alb_sg_id.value
     bastion_sg_id = data.aws_ssm_parameter.bastion_sg_id.value
+    mongodb_sg_id = data.aws_ssm_parameter.mongodb_sg_id.value
     common_tags = {
         Project = var.project_name
         Environment = var.environment

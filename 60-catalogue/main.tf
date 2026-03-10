a3548044f54472b6492c7ff16a88ffb86655fa51
sgraju <sgraju.tibco@gmail.com>
2026-03-10 21:45:31 +0400

    60-catalogue

--- 60-catalogue/main.tf
+++ 60-catalogue/main.tf
@@ -41,3 +41,16 @@ resource "aws_instance" "catalogue" {
       ]
     }
 }
+
+# stop the instance to take AMI
+resource "aws_ec2_instance_state" "catalogue" {
+  instance_id = aws_instance.catalogue.id
+  state = "stopped"
+  depends_on = [ terraform_data.catalogue ]
+}
+
+resource "aws_ami_from_instance" "catalogue" {
+  name = "${local.common_name_suffix}-catalogue-ami"
+  source_instance_id = aws_instance.catalogue.id
+  depends_on = [ aws_ec2_instance_state.catalogue ]
+}

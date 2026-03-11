0bee20c0ab48d1531c134fe32e923a2c79863bb6
sgraju <sgraju.tibco@gmail.com>
2026-03-11 23:31:11 +0400

    60-catalogue

--- 60-catalogue/locals.tf
+++ 60-catalogue/locals.tf
@@ -2,7 +2,7 @@ locals {
     ami_id = data.aws_ami.joindevops.id
     common_name_suffix = "${var.project_name}-${var.environment}" #roboshop-dev
     private_subnet_id = split("," , data.aws_ssm_parameter.private_subnet_id.value)[0]
-    private_subnet_ids = split("," , data.aws_ssm_parameter.private_subnet_ids.value)[0]
+    private_subnet_ids = split("," , data.aws_ssm_parameter.private_subnet_ids.value)
     catalogue_sg_id = data.aws_ssm_parameter.catalogue_sg_id.value
     vpc_id = data.aws_ssm_parameter.vpc_id.value
     backend_alb_listener_arn = data.aws_ssm_parameter.backed_alb_listener_arn.value

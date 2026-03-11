351b710756158949599b07f8c3f025aed1198103
sgraju <sgraju.tibco@gmail.com>
2026-03-11 20:58:29 +0400

    50-backend-alb

--- 50-backend-alb/main.tf
+++ 50-backend-alb/main.tf
@@ -40,8 +40,8 @@ resource "aws_route53_record" "backend_alb" {
   type = "A"
 
   alias {
-    name    = aws_lb.backend_alb_dns_name
-    zone_id = aws_lb.backend_alb_zone_id
+    name    = aws_lb.backend_alb.dns_name
+    zone_id = aws_lb.backend_alb.zone_id
     evaluate_target_health = true
   }
 }

b7c5cf8839936b004e9f5a420b23c4a89b9e2f2d
sgraju <sgraju.tibco@gmail.com>
2026-03-11 18:47:02 +0400

    20-bastion

--- 20-bastion/main.tf
+++ 20-bastion/main.tf
@@ -4,7 +4,14 @@ resource "aws_instance" "bastion" {
   vpc_security_group_ids = [local.bastion_sg_id]
   subnet_id = local.public_subnet_id
   iam_instance_profile = aws_iam_instance_profile.bastion.name
+ # need more memory for terraform
+  root_block_device {
+  volume_size = 50
+  volume_type = "gp3" # or "gp2" depending on your preferene
+ }
+
   user_data = file("bastion.sh")
+  
   tags = merge (
     local.common_tags,
     {

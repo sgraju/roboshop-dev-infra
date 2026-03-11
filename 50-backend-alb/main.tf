dc1f3571c57404f7065e6583118a815df5383bcf
sgraju <sgraju.tibco@gmail.com>
2026-03-11 20:27:39 +0400

    50-backend-alb

--- 50-backend-alb/main.tf
+++ 50-backend-alb/main.tf
@@ -33,3 +33,15 @@ resource "aws_lb_listener" "backend_alb" {
     }
   }
 }
+
+resource "aws_route53_record" "backend_alb" {
+  zone_id = var.zone_id
+  name = "*.backend-alb-${var.environment}.${var.domain_name}"
+  type = "A"
+
+  alias {
+    name    = aws_lb.backend_alb_dns_name
+    zone_id = aws_lb.backend_alb_zone_id
+    evaluate_target_health = true
+  }
+}
--- 50-backend-alb/variables.tf
+++ 50-backend-alb/variables.tf
@@ -5,3 +5,11 @@ variable "project_name" {
 variable "environment" {
   default = "dev"
 }
+
+variable "zone_id" {
+  default = "Z0732355102QE6GB8XDYY"
+}
+
+variable "domain_name" {
+  default = "sgrdevsecops.fun"
+}

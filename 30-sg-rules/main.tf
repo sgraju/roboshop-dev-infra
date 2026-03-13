20105c3a92737c017fb369938f66ff26761084d1
sgraju <sgraju.tibco@gmail.com>
2026-03-13 19:01:53 +0400

    roboshop1

--- 30-sg-rules/main.tf
+++ 30-sg-rules/main.tf
@@ -75,7 +75,16 @@ resource "aws_security_group_rule" "catalogue_backend_alb" {
   type = "ingress"
   security_group_id = local.catalogue_sg_id
   source_security_group_id  = local.backend_alb_sg_id
-  from_port  = 27017
+  from_port  = 8080
   protocol   = "tcp"
-  to_port    = 27017
+  to_port    = 8080
+}
+
+resource "aws_security_group_rule" "frontend_alb_public" {
+  type = "ingress"
+  security_group_id = local.frontend_alb_sg_id
+  cidr_blocks = ["0.0.0.0/0"]
+  from_port  = 443
+  protocol   = "tcp"
+  to_port    = 443
 }

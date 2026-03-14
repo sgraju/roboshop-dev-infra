e3f29bb1ce069d22abc845793891b8ed973a767e
sgraju <sgraju.tibco@gmail.com>
2026-03-14 10:45:17 +0400

    roboshop1

---
+++ 70-acm/variables.tf
@@ -0,0 +1,15 @@
+variable "project_name" {
+    default = "roboshop1"
+}
+
+variable "environment" {
+  default = "dev"
+}
+
+variable "domain_name" {
+  default = "sgrdevsecops.fun"
+}
+
+variable "zone_id" {
+  default = "Z0732355102QE6GB8XDYY"
+}

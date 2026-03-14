2c771aaa8241d954ed18a8d7c32c32590a0ecccd
sgraju <sgraju.tibco@gmail.com>
2026-03-14 10:59:11 +0400

    roboshop1

--- 80-frontend-alb/data.tf
+++ 80-frontend-alb/data.tf
@@ -9,3 +9,7 @@ data "aws_ssm_parameter" "frontend_alb_sg_id" {
 data "aws_ssm_parameter" "public_subnet_ids" {
   name = "/${var.project_name}/${var.environment}/public_subnet_ids"
 }
+
+data "aws_ssm_parameter" "frontend_alb_certificate_arn" {
+  name = "/${var.project_name}/${var.environment}/frontend_alb_certificate_arn"
+}

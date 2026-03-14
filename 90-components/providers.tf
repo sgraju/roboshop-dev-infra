820526a2d1afa093ea69bcef03dd61535b7675c8
sgraju <sgraju.tibco@gmail.com>
2026-03-14 19:48:47 +0400

    roboshop-dev-infra

--- 90-components/providers.tf
+++ 90-components/providers.tf
@@ -7,7 +7,7 @@ terraform {
   }
 
   backend "s3" {
-    bucket = "remote-state-86s-dev"
+    bucket = "sgr-remote-state-dev"
     key    = "roboshop-dev-components"
     region = "us-east-1"
     use_lockfile = true

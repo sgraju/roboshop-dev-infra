fa8d7cb404869dcd4583ede2457d8779295a4f88
sgraju <sgraju.tibco@gmail.com>
2026-03-14 11:19:37 +0400

    roboshop1

--- 20-bastion/bastion.sh
+++ 20-bastion/bastion.sh
@@ -8,3 +8,14 @@ xfs_growfs /home
 sudo yum install -y yum-utils
 sudo yum-config-manager --add-repo https://rpm.releases.hashicorp.com/RHEL/hashicorp.repo
 sudo yum -y install terraform
+
+
+# sudo lvreduce -r -L 6G /dev/mapper/RootVG-rootVol
+
+# creating databases
+cd /home/ec2-user
+git clone https://github.com/sgraju/roboshop-dev-infra.git
+chown ec2-user:ec2-user -R roboshop-dev-infra
+cd roboshop-dev-infra/40-databases
+terraform init
+terraform apply -auto-approve
